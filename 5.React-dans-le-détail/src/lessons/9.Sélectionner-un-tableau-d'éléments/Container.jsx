830ebcbd37d9b3b81f104f48cc8c62e36e60e8af
Lionel EKOM <ekomlionel@gmail.com>
2024-06-02 21:29:41 +0100

    "Added useRef and ref callback to store input elements in an array"

--- 5.React-dans-le-détail/src/lessons/9.Sélectionner-un-tableau-d'éléments/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/9.Sélectionner-un-tableau-d'éléments/Container.jsx
@@ -1,11 +1,21 @@
+import { useRef } from "react"
+
 export default function Container() {
 
+  const inputsRef = useRef([])
+
+  function addInputRef(el) {
+    if(el && !inputsRef.current.includes(el)) {
+      inputsRef.current.push(el)
+    }
+    console.log(inputsRef);
+  }
   return (
     <div>
       <h1>Sélectionner un tableau d'éléments</h1>
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" />
-        <input type="text" name="" id="" />
+        <input ref={addInputRef} type="text" name="" id="" />
+        <input ref={addInputRef} type="text" name="" id="" />
+        <input ref={addInputRef} type="text" name="" id="" />
     </div>
   )
 }
