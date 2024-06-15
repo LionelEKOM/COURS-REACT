031c0007f614514b55bf754e3399ea2a0cb09bda
Lionel EKOM <ekomlionel@gmail.com>
2024-06-16 00:19:57 +0100

    "Updated App.jsx to import Container from lessons/14.Hook-personnalisé instead of lessons/12.useMemo, and modified Container.jsx in lessons/13.useReducer to use useReducer hook with increment, decrement, multiply, and divide actions

--- 5.React-dans-le-détail/src/lessons/13.useReducer/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/13.useReducer/Container.jsx
@@ -1,7 +1,34 @@
+import { useReducer } from "react"
+
 export default function Container() {
 
-  return <div>
-    <h1>Valeur du compteur : </h1>
+  const[count, dispatch] = useReducer(reducer, 0)
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'INCREMENT':
+        return state + 1
+      case 'DECREMENT':
+        return state - 1
+      case 'Multi':
+        return state * 2
+      case 'div':
+        return state / 5
+    }
+  }
 
+  return <div>
+    <h1>Valeur du compteur : {count}</h1>
+      <button
+      onClick={() => dispatch({ type: "INCREMENT", payload: 2 })}
+      >Incremente</button>
+      <button
+      onClick={() => dispatch({ type: "DECREMENT", payload: 2 })}
+      >DECREMENT</button>
+      <button
+      onClick={() => dispatch({ type: "Multi", payload: 2 })}
+      >Multi</button>
+      <button
+      onClick={() => dispatch({ type: "div", payload: 2 })}
+      >div</button>
   </div>
 }
