69e64366696195704fe9e5f35e25f69f63dd89d2
Lionel EKOM <ekomlionel@gmail.com>
2024-07-06 09:37:44 +0100

    "Added Counter component to App and wrapped App with Provider from react-redux in main.jsx"

---
+++ 9.React-Redux/src/pages/Counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function Counter() {
+
+    const counter = useSelector(state => state.counter)
+
+  return (
+    <div>
+      <h1 className="text-slate-100 text-3xl">
+        Counter value : {counter.value}
+      </h1>
+    </div>
+  )
+}
